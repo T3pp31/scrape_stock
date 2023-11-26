1513d9dcca24a8173714fd1e1f45b19f549e61e5
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2023-11-27 00:55:33 +0900

    add input_txt

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,14 @@
 use anyhow::Result;
 use reqwest::blocking;
 use scraper::{Html, Selector};
-use std::fs::{create_dir_all, File};
-use std::io::{BufWriter, Write};
+use std::fs::{create_dir_all, remove_file, File};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::path::{self, Path};
 use xcss::xcss::xcss;
 
 const URL: &str = "https://kabutan.jp/stock/?code=";
 
 fn main() {
-    let stock_id = "4581";
-    let document = search_stock(stock_id).expect("Failed to fetch stock information");
-    let stock_name =
-        get_stock_info(&document, "//*[@id='stockinfo_i1']/div[1]/h2").unwrap_or_else(|err| {
-            eprintln!("Error getting stock name: {}", err);
-            String::from("N/A")
-        });
-
-    let stock_price = get_stock_info(&document, "//*[@id='stockinfo_i1']/div[2]/span[2] ")
-        .unwrap_or_else(|err| {
-            eprintln!("Error getting stock price: {}", err);
-            String::from("N/A")
-        });
-    let per = get_stock_info(&document, "//*[@id='stockinfo_i3']/table/tbody/tr[1]/td[1]")
-        .unwrap_or_else(|err| {
-            eprintln!("Error getting per: {}", err);
-            String::from("N/A")
-        });
-
-    let pbr = get_stock_info(&document, "//*[@id='stockinfo_i3']/table/tbody/tr[1]/td[2]")
-        .unwrap_or_else(|err| {
-            eprintln!("Error getting pbr: {}", err);
-            String::from("N/A")
-        });
-    let return_per = get_stock_info(&document, "//*[@id='stockinfo_i3']/table/tbody/tr[1]/td[3]")
-        .unwrap_or_else(|err| {
-            eprintln!("Error getting return_per: {}", err);
-            String::from("N/A")
-        });
-
-    let predict_return = get_stock_info(
-        &document,
-        "//*[@id='kobetsu_right']/div[3]/table/tbody/tr[3]/th",
-    )
-    .unwrap_or_else(|err| {
-        eprintln!("Error getting predict_return: {}", err);
-        String::from("N/A")
-    });
-
-    let earning_per_share = get_stock_info(
-        &document,
-        "//*[@id='kobetsu_right']/div[3]/table/tbody/tr[3]/td[4]",
-    )
-    .unwrap_or_else(|err| {
-        eprintln!("Error getting earning_per_share: {}", err);
-        String::from("N/A")
-    });
-
-    let amount_return = get_stock_info(
-        &document,
-        "//*[@id='kobetsu_right']/div[3]/table/tbody/tr[3]/td[5]",
-    )
-    .unwrap_or_else(|err| {
-        eprintln!("Error getting amount_return: {}", err);
-        String::from("N/A")
-    });
-
-    println!("Stock Name: {}", stock_name);
-    println!("Stock Price: {}", stock_price);
-    println!("per:{}", per);
-    println!("pbr:{}", pbr);
-    println!("return_per:{}", return_per);
-    println!("1株益{}", earning_per_share);
-    println!("{}:{}", predict_return, amount_return);
-
-    let data = format!(
-        "{},{},{},{},{},{},{},{},{}",
-        stock_id,
-        stock_name,
-        stock_price,
-        per,
-        pbr,
-        return_per,
-        earning_per_share,
-        predict_return,
-        amount_return
-    )
-    .to_string();
-
     //ファイルパスの指定
     let path = Path::new("output");
 
@@ -95,20 +16,112 @@ fn main() {
 
     //ファイルを開いたことがなかったらfirst_openを実行
 
-    //ファイルが存在するかを調べる
-
-    //if !Path::is_file(test_path) {
-    //    first_open(path);
-    // }
+    //ファイルが存在するかを調べる.ファイルが存在すると上書き保存がうまくいかないので，一度削除してから作成
 
     let mut writer = if !Path::is_file(test_path) {
         first_open(path)
     } else {
+        remove_file(test_path);
+        first_open(path);
         open_csv(path)
     }
     .unwrap();
 
-    write_to_csv(&data, &mut writer);
+    let stock_ids = read_stock_id().expect("Failed to read input.txt");
+    for stock_id in stock_ids {
+        let document = search_stock(&stock_id).expect("Failed to fetch stock information");
+        let stock_name = get_stock_info(&document, "//*[@id='stockinfo_i1']/div[1]/h2")
+            .unwrap_or_else(|err| {
+                eprintln!("Error getting stock name: {}", err);
+                String::from("N/A")
+            });
+
+        let stock_price = get_stock_info(&document, "//*[@id='stockinfo_i1']/div[2]/span[2] ")
+            .unwrap_or_else(|err| {
+                eprintln!("Error getting stock price: {}", err);
+                String::from("N/A")
+            });
+        let per = get_stock_info(&document, "//*[@id='stockinfo_i3']/table/tbody/tr[1]/td[1]")
+            .unwrap_or_else(|err| {
+                eprintln!("Error getting per: {}", err);
+                String::from("N/A")
+            });
+
+        let pbr = get_stock_info(&document, "//*[@id='stockinfo_i3']/table/tbody/tr[1]/td[2]")
+            .unwrap_or_else(|err| {
+                eprintln!("Error getting pbr: {}", err);
+                String::from("N/A")
+            });
+        let return_per =
+            get_stock_info(&document, "//*[@id='stockinfo_i3']/table/tbody/tr[1]/td[3]")
+                .unwrap_or_else(|err| {
+                    eprintln!("Error getting return_per: {}", err);
+                    String::from("N/A")
+                });
+
+        let predict_return = get_stock_info(
+            &document,
+            "//*[@id='kobetsu_right']/div[3]/table/tbody/tr[3]/th",
+        )
+        .unwrap_or_else(|err| {
+            eprintln!("Error getting predict_return: {}", err);
+            String::from("N/A")
+        });
+
+        let earning_per_share = get_stock_info(
+            &document,
+            "//*[@id='kobetsu_right']/div[3]/table/tbody/tr[3]/td[4]",
+        )
+        .unwrap_or_else(|err| {
+            eprintln!("Error getting earning_per_share: {}", err);
+            String::from("N/A")
+        });
+
+        let amount_return = get_stock_info(
+            &document,
+            "//*[@id='kobetsu_right']/div[3]/table/tbody/tr[3]/td[5]",
+        )
+        .unwrap_or_else(|err| {
+            eprintln!("Error getting amount_return: {}", err);
+            String::from("N/A")
+        });
+
+        println!("Stock Name: {}", stock_name);
+        println!("Stock Price: {}", stock_price);
+        println!("per:{}", per);
+        println!("pbr:{}", pbr);
+        println!("return_per:{}", return_per);
+        println!("1株益{}", earning_per_share);
+        println!("{}:{}", predict_return, amount_return);
+
+        let data = format!(
+            "{},{},{},{},{},{},{},{},{}",
+            stock_id,
+            stock_name,
+            stock_price,
+            per,
+            pbr,
+            return_per,
+            earning_per_share,
+            predict_return,
+            amount_return
+        )
+        .to_string();
+
+        write_to_csv(&data, &mut writer);
+    }
+}
+
+fn read_stock_id() -> Result<Vec<String>, io::Error> {
+    //保存用のリストを作成する
+    let mut stock_ids: Vec<String> = Vec::new();
+
+    // .txtファイルの中身を1行ずつ取り出す
+    for result in BufReader::new(File::open("input.txt")?).lines() {
+        let line = result?;
+        stock_ids.push(line);
+    }
+    Ok(stock_ids)
 }
 
 fn search_stock(stock_id: &str) -> Result<Html, reqwest::Error> {
